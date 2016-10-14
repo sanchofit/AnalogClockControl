d3493fa59e28c275780e8978a361146a392b9750
mesutgenc <mesutgenc@MRK-MESUTGENC>
2016-10-14 09:28:56 +0300

    v1.1

--- AnalogClockControl/Resources/Resource.Designer.cs
+++ AnalogClockControl/Resources/Resource.Designer.cs
@@ -57,6 +57,22 @@ namespace AnalogClockControl
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int analogClock1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
